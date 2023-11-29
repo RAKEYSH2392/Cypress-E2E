11e486abee03f7e15f42be24bee7f1cf3c25afd2
Rakesh Kumar2 <rakesh.kumar2@amantyatech.com>
2023-11-29 20:59:28 +0530

    update for video

--- cypress.config.js
+++ cypress.config.js
@@ -13,5 +13,14 @@ module.exports = defineConfig({
   },
   env:{
     URL:'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
-  }
+  },
+  // Add the following configuration for video recording
+  video: true,
+  videoUploadOnPasses: false,
+  videoCompression: 32,
+  videoUploadOnError: true,
+  videoS3Region: 'your-region',
+  videoS3AccessKeyId: 'your-access-key-id',
+  videoS3SecretAccessKey: 'your-secret-access-key',
+  videoS3Bucket: 'your-s3-bucket',
 });
